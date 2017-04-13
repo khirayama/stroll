0ab2d749f521803e794c9a62c085b206094d2f71
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2017-04-14 07:56:54 +0900

    Create routing in api server

--- api/src/index.js
+++ api/src/index.js
@@ -2,10 +2,42 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+const loginStatuses = {
+  NOT_AUTHORIZED: '__NOT_AUTHORIZED',
+  CONNECTED: '__CONNECTED',
+};
 
+// handlers
+function loginStatusHandler(req, res) {
+  res.json({
+    status: loginStatuses.NOT_AUTHORIZED,
+  });
+}
+
+function createUserHandler(req, res) {
+  User.findOrCreate().then(user => {
+    Token.find({
+      where: {provider, uid}
+    })
+  });
+  res.json({
+    accessToken: 'ACCESS_TOKEN',
+  });
+}
+
+// router
+const router = new express.Router('');
+
+router.use('/api', new express.Router()
+  .use('/v1', new express.Router()
+    .get('/login-status', loginStatusHandler)
+    .post('/users', createUserHandler)
+  )
+);
+
+app.use(router);
+
+// main
 app.listen(3000, () => {
   console.log('Example app listening on port 3000!');
 });
