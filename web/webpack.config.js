33d99a795c8941bb58c8afbb590ab527269d0835
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2017-04-17 22:41:25 +0900

    Create webpack.config.js

---
+++ web/webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  entry: './src/client.js',
+  output: {
+    path: path.resolve(__dirname, 'dist', 'public'),
+    filename: 'bundle.js'
+  },
+  resolve: {
+    modules: [
+      path.join(__dirname, "src"),
+      "node_modules",
+    ],
+  },
+  module: {
+    rules: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      use: 'babel-loader',
+    }],
+  },
+  devtool: 'inline-source-map',
+};
