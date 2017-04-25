e52adeea5f4cd88fa5fc3bf7a0ac98de339fb513
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2017-04-26 07:24:24 +0900

    Add route for verification

--- web/src/routes.js
+++ web/src/routes.js
@@ -1,9 +1,37 @@
 import HomePage from './components/home-page';
 
+import React from 'react';
+import {Link} from './components/navigator';
+
+class TestPage extends React.Component {
+  render() {
+    return (
+      <section>
+        <div>Test</div>
+        <Link href="/">to home</Link>
+      </section>
+    );
+  }
+}
+
 export const routes = [{
   path: '/',
   key: 'HOME_PAGE',
-  props: null,
+  data: () => {
+    return new Promise(resolve => {
+      resolve();
+    });
+  },
   title: 'HOME | Stroll',
   component: HomePage,
+}, {
+  path: '/test',
+  key: 'TEST_PAGE',
+  data: () => {
+    return new Promise(resolve => {
+      resolve();
+    });
+  },
+  title: 'TEST | Stroll',
+  component: TestPage,
 }];
