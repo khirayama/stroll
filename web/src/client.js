df44adc0711afe18a31e59c3a52553053debf6dc
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2017-05-07 07:28:35 +0900

    Create reducer layer

--- web/src/client.js
+++ web/src/client.js
@@ -5,13 +5,14 @@ import {createStore} from '@khirayama/circuit';
 
 import Router from './router';
 import {routes} from './router/routes';
+import {reducer} from './reducer';
 
 import Navigator from './components/navigator';
 
 window.addEventListener('DOMContentLoaded', () => {
   console.log(`Start app at ${new Date()}.`);
 
-  const store = createStore(window.state);
+  const store = createStore(window.state, reducer);
 
   const router = new Router(routes);
   router.push(location.pathname);
---
+++ web/src/reducer/index.js
@@ -0,0 +1,3 @@
+export function reducer(state, action) {
+  console.log(state, action);
+}
