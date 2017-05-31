21ab8aad8c684d6d57b393383e013c4f1347aeed
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2017-05-31 09:33:58 +0900

    Allow cross origin request

--- api/src/index.js
+++ api/src/index.js
@@ -102,6 +102,11 @@ router.use('/api', new express.Router()
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  next();
+});
 app.use(router);
 
 // main
