5a88f17fd34f15db679c324b5bd38e3a72ce7ec3
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2017-06-02 08:42:24 +0900

    Change " to \'

--- api/src/index.js
+++ api/src/index.js
@@ -103,8 +103,8 @@ router.use('/api', new express.Router()
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
   next();
 });
 app.use(router);
